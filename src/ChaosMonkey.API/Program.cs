8881bba01f49debe0e8d034b1a5fa4adb4dc1820
Tom Kerkhove <kerkhove.tom@gmail.com>
2024-04-28 20:21:32 +0200

    WIP
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/ChaosMonkey.API/Program.cs
+++ src/ChaosMonkey.API/Program.cs
@@ -1,7 +1,9 @@
 using System.Text.Json.Serialization;
+using Azure.Identity;
 using ChaosMonkey.API;
 using ChaosMonkey.API.Integrations;
 using ChaosMonkey.API.Repositories;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Logging.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,6 +36,19 @@ builder.Services.AddSingleton<AzureResourceManagerClient>(services =>
 });
 builder.Services.AddTransient<ApiManagementRepository>();
 builder.Services.AddHostedService<ShutdownGatewayWorker>();
+// Check https://learn.microsoft.com/en-gb/azure/azure-monitor/app/ilogger?tabs=dotnet6 
+//https://learn.microsoft.com/en-gb/azure/azure-monitor/app/asp-net-core?tabs=netcorenew
+// https://github.com/microsoft/ApplicationInsights-dotnet/blob/main/docs/concepts.md
+// https://www.nuget.org/packages/Microsoft.ApplicationInsights/2.22.0
+builder.Services.Configure<TelemetryConfiguration>(config =>
+{
+       var credential = new DefaultAzureCredential();
+       config.SetAzureTokenCredential(credential);
+});
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+{
+    ConnectionString = "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://xxxx.applicationinsights.azure.com/"
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/ChaosMonkey.API/Repositories/ApiManagementRepository.cs
+++ src/ChaosMonkey.API/Repositories/ApiManagementRepository.cs
@@ -8,28 +8,30 @@ namespace ChaosMonkey.API.Repositories
     public class ApiManagementRepository
     {
         readonly AzureResourceManagerClient _armClient;
+        readonly ILogger _logger;
 
-        public ApiManagementRepository(AzureResourceManagerClient armClient)
+        public ApiManagementRepository(AzureResourceManagerClient armClient, ILogger logger)
         {
             _armClient = armClient;
+            _logger = logger;
         }
 
         public async Task ManageGatewayInRegion(string subscriptionId, string resourceGroupName, string serviceName, AzureLocation azureRegionInfo, bool isGatewayEnabled)
         {
-            var serviceInfo = await this._armClient.GetServiceInfo(subscriptionId, resourceGroupName, serviceName);
-
-            var secondaryLocationInRegion = serviceInfo?.AdditionalLocations.FirstOrDefault(x => x.Location == azureRegionInfo);
+            var serviceInfo = await _armClient.GetServiceInfo(subscriptionId, resourceGroupName, serviceName);
             if (serviceInfo?.Location == azureRegionInfo)
             {
-                await this._armClient.UpdateService(subscriptionId, resourceGroupName, serviceName, patch => patch.DisableGateway = isGatewayEnabled);                
+                await _armClient.UpdateService(subscriptionId, resourceGroupName, serviceName, patch => patch.DisableGateway = isGatewayEnabled);
+                _logger.LogInformation("Changed state of primary region ({azureRegionInfo}) to {newState}", azureRegionInfo, isGatewayEnabled ? "enabled" : "disabled");
             }
-            else if (secondaryLocationInRegion != null)
+            else if (serviceInfo?.AdditionalLocations.Any(x => x.Location == azureRegionInfo) == true)
             {
-                await this._armClient.UpdateService(subscriptionId, resourceGroupName, serviceName, patch =>
+                await _armClient.UpdateService(subscriptionId, resourceGroupName, serviceName, patch =>
                 {
                     foreach (var matchingSecondaryLocation in patch.AdditionalLocations.Where(x => x.Location == azureRegionInfo))
                     {
                         matchingSecondaryLocation.DisableGateway = isGatewayEnabled;
+                        _logger.LogInformation("Changed state of secondary location ({azureRegionInfo}) to {newState}", azureRegionInfo, isGatewayEnabled ? "enabled" : "disabled");
                     }
                 });
             }
@@ -41,7 +43,7 @@ namespace ChaosMonkey.API.Repositories
 
         public async Task<List<GatewayInfo>> Get(string subscriptionId, string resourceGroupName, string serviceName)
         {
-            var serviceInfo = await this._armClient.GetServiceInfo(subscriptionId, resourceGroupName, serviceName);
+            var serviceInfo = await _armClient.GetServiceInfo(subscriptionId, resourceGroupName, serviceName);
 
             var gatewayInfo = new List<GatewayInfo>
             {
